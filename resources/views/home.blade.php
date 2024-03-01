8468c88598e38cf406f560b10e003d57c5230d8a
Admin <admin@vinasupport.com>
2024-03-01 17:44:48 +0700

    finish bai3

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,12 @@
 <h1>Hoc Lap trinh tai unicode</h1>
+<?php 
+echo date('Y-m-d H:i:s');
+if (env('APP_ENV') == 'production'){
+    // call api live
+    echo "call api live";
+} else{
+    // call api sandbox
+    echo "call api sandbox";
+}
+echo config('app.env')
+?>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
-
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Models\User;
+use App\Http\Controllers\FormController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,8 +19,47 @@ Route::get('/', function () {
     // return view('home');
 });
 Route::get('/unicode', function(){
-    return view('home');
+    $user = new User();
+    $allUser = $user::all();
+    dd($allUser);
+    // return view('home');
 });
 Route::get('/product', function(){
     return view('product');
 });
+
+// Route::get('/admin/product', function(Request $request){
+//     return "Path: " . $request->path();
+// });
+
+// Route::get('/admin/product', function(Request $request){
+//     return "Url: " . $request->url();
+// });
+// Route::get('/admin/product', function(Request $request){
+//     return "Full Url: " . $request->fullUrl();
+// });
+// Route::get('/admin/product', function(Request $request){
+//     return "Full Url: " . $request->fullUrlWithQuery(['age' => '21']);
+// });
+// Route::get('/admin/product', function(Request $request){
+//     if ($request->is('admin/*')){
+//         return "path la admin";
+//     } else {
+//         return "path khong la admin";
+//     }
+// });
+// Route::get('/admin/product', function(Request $request){
+//     return "Method: " . $request->method();
+// });
+
+// Route::get('/admin/product', function(Request $request){
+//    echo "Method: " . $request->method() . "<br>";
+//   if ($request->isMethod('get')){
+//     echo "This is GET method HTTP.";
+//   }
+// });
+// Route::get('/userIp', function(Request $request){
+//     return "IP address: " . $request->ip();
+// });
+Route::get('/index', [FormController::class, 'index']);
+Route::post('/post', [FormController::class, 'post']);
